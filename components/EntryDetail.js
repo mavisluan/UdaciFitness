1a3356505dc238954ce82304b0df17739b3f559e
Yi Angelov <mavisluan83@gmail.com>
2018-06-11 06:49:49 -0700

    style EntryDetail

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
+import { timeToString, getDailyReminderValue } from '../utils/helpers'
+import MetricCard from './MetricCard'
+import { white } from '../utils/helpers'
 
-export default class EntryDetail extends Component {
+class EntryDetail extends Component {
   static navigationOptions = ({ navigation }) => {
-    // entryId is the selected day. eg.' 2018-06-05'
     const { entryId } = navigation.state.params
-    console.log(entryId)
+  // console.log(entryId)
+
     const year = entryId.slice(0, 4)
-    const month = entryId.slice(5, 7)
+    const month = entryId.slice(5,7)
     const day = entryId.slice(8)
 
     return {
@@ -15,10 +19,34 @@ export default class EntryDetail extends Component {
     }
   }
   render() {
+    const { metrics } = this.props
+    // console.log(this.props)
     return (
-      <View>
+      <View style={styles.container}>
+        <MetricCard metrics={metrics} />
         <Text>Entry Detail - {JSON.stringify(this.props.navigation.state.params.entryId)}</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15,
+  },
+})
+
+function mapStateToProps (state, { navigation }) {
+  const { entryId } = navigation.state.params
+
+  return {
+    entryId,
+    metrics: state[entryId],
+  }
+}
+
+export default connect(
+  mapStateToProps,
+)(EntryDetail)
