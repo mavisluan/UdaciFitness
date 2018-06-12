280f4526a2c3be0721da2f3a11847c42384b145c
Yi Angelov <mavisluan83@gmail.com>
2018-06-11 21:01:46 -0700

    finish navigation

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -14,6 +14,7 @@ import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
 import { purple, white } from '../utils/colors'
+// import { NavigationActions } from 'react-navigation'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -71,6 +72,7 @@ class AddEntry extends Component {
     this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
 
     // Navigate to home
+    // this.toHome()
 
     submitEntry({ key, entry })
 
@@ -84,9 +86,14 @@ class AddEntry extends Component {
     }))
 
     // Route to Home
+    // this.toHome()
 
     removeEntry(key)
   }
+
+  // toHome = () => {
+  //   this.props.navigation.dispatch(NavigationActions.back({key: 'AddEntry'}))
+  // }
   render() {
     const metaInfo = getMetricMetaInfo()
 
--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -4,6 +4,9 @@ import { connect } from 'react-redux'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
 import MetricCard from './MetricCard'
 import { white } from '../utils/helpers'
+import TextButton from './TextButton'
+import { addEntry } from '../actions'
+import { removeEntry } from '../utils/api'
 
 class EntryDetail extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -18,13 +21,28 @@ class EntryDetail extends Component {
       title: `${month}/${day}/${year}`
     }
   }
+
+  reset = () => {
+    const { remove, goBack, entryId } = this.props
+
+    remove()
+    goBack()
+    removeEntry(entryId)
+  }
+
+  shouldComponentUpdate( nextProps) {
+    return nextProps.metrics !== null && !nextProps.metrics.today
+  }
+
   render() {
     const { metrics } = this.props
     // console.log(this.props)
     return (
       <View style={styles.container}>
         <MetricCard metrics={metrics} />
-        <Text>Entry Detail - {JSON.stringify(this.props.navigation.state.params.entryId)}</Text>
+        <TextButton style={{margin: 20}} onPress={this.reset} >
+          RESET
+        </TextButton>
       </View>
     )
   }
@@ -47,6 +65,18 @@ function mapStateToProps (state, { navigation }) {
   }
 }
 
+function mapDispatchToProps (dispatch, { navigation }) {
+  const { entryId } = navigation.state.params
+
+  return {
+    remove: () => dispatch(addEntry({
+      [entryId]: timeToString() === entryId
+        ? getDailyReminderValue()
+        : null
+    })),
+    goBack: () => navigation.goBack()
+  }
+}
 export default connect(
-  mapStateToProps,
+  mapStateToProps,mapDispatchToProps
 )(EntryDetail)
